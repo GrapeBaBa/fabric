f8236285823313247e9c87e0c30ea35ca514ea4f
grapebaba <281165273@qq.com>
2021-06-07 15:36:10 +0800

    Change tx consumer count
    Signed-off-by: grapebaba <281165273@qq.com>

--- orderer/consensus/etcdraft/chain.go
+++ orderer/consensus/etcdraft/chain.go
@@ -593,7 +593,7 @@ func (c *Chain) run() {
 
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	// multiple propose goroutines
-	for i := 0; i < runtime.NumCPU()/2; i++ {
+	for i := 0; i < runtime.NumCPU(); i++ {
 		go func() {
 			ticking := false
 			timer := c.clock.NewTimer(time.Second)
